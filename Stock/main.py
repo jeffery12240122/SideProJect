5bb8a0267effd74e11ace853cd723f261df52273
Jeffery Lin - TW (SW) <JefferyL@supermicro.com>
2024-02-26 18:17:32 +0800

    [Feature] Reivsed the code flow
    [Description]
    Add stockNum()

--- Stock/main.py
+++ Stock/main.py
@@ -1,18 +1,12 @@
 import reptile
+stock_code = reptile.stockNum()
+tree = tree = reptile.getUrl(stock_code)
 
-name = reptile.getName()[0].strip()
-price = reptile.getPrice()[0].strip()
-up = reptile.getUpDown()[0].strip()
-percent = reptile.getPercentage()[0].strip()[1:-1]
 
-if reptile.getUrl()[0].status_code == 200:
-    if reptile.getName():
-        print(name + '股價：', price)
-        print('漲跌:', up)
-        print('漲跌幅:', percent)
-    else:
-        print('無法找到此股。')
-else:
-    print('404 Error!!!。')
+print(reptile.getName(tree) + '股價：', reptile.getPrice(tree))
+print('漲跌:', reptile.getUpDown(tree))
+print('漲跌幅:', reptile.getPercentage(tree))
+
+
 
 
--- Stock/reptile.py
+++ Stock/reptile.py
@@ -1,24 +1,38 @@
 import requests
 from lxml import html
-stock_num = (input("請輸入股價代碼:"))
-def getUrl():
+def stockNum():
+    stock_num = (input("請輸入股價代碼:"))
+    return stock_num
+def getUrl(stock_num):
     url = 'https://tw.stock.yahoo.com/q/q?s='+stock_num
     # 發送 GET 請求並獲取內容
     response = requests.get(url)
     tree = html.fromstring(response.content)
-    return response,tree
+    return tree
 
-def getPrice():
-    return getUrl()[1].xpath('//*[@id="main-0-QuoteHeader-Proxy"]/div/div[2]/div[1]/div/span[1]/text()')
+def getPrice(tree):
+    price = tree.xpath('//*[@id="main-0-QuoteHeader-Proxy"]/div/div[2]/div[1]/div/span[1]/text()')
+    return float(price[0].replace(',', ''))
     
 
-def getName():
-    return getUrl()[1].xpath('//*[@id="main-0-QuoteHeader-Proxy"]/div/div[1]/h1/text()')
+def getName(tree):
+    return tree.xpath('//*[@id="main-0-QuoteHeader-Proxy"]/div/div[1]/h1/text()')[0].strip()
 
 
-def getUpDown():
-    return getUrl()[1].xpath('//*[@id="main-0-QuoteHeader-Proxy"]/div/div[2]/div[1]/div/span[2]/text()')
+def getUpDown(tree):
+    return float(tree.xpath('//*[@id="main-0-QuoteHeader-Proxy"]/div/div[2]/div[1]/div/span[2]/text()')[0].strip())
 
-def getPercentage():
-    return getUrl()[1].xpath('//*[@id="main-0-QuoteHeader-Proxy"]/div/div[2]/div[1]/div/span[3]/text()')
+def getPercentage(tree):
+
+    percentage = tree.xpath('//*[@id="main-0-QuoteHeader-Proxy"]/div/div[2]/div[1]/div/span[3]/text()')
+    updown = tree.xpath('//*[@id="main-0-QuoteHeader-Proxy"]/div/div[2]/div[1]/div/span[2]/span/@style')
+    if '#ff333a' in updown:
+        return float(0) + float(percentage[0].replace('%', '')[1:-1])
+    elif '#00ab5e' in updown:
+        return float(0) - float(percentage[0].replace('%', '')[1:-1])
+    else:
+        return float(percentage[0].replace('%', '')[1:-1])
+
+    
+
 
